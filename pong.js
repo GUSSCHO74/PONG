84d558a7e1cf961521fe220b7f76a90a9246ec2b
GUSSCH <90836568+GUSSCHO74@users.noreply.github.com>
2022-04-26 14:06:15 +0200

    Add files via upload

---
+++ pong.js
@@ -0,0 +1,16 @@
+var canvas = document.createElement("canvas");
+canvas.style = "position:absolute; left: 50%; width: 400px; height 100px; margin-left: -200px; border:1px solid #000000;";
+console.log(canvas)
+
+document.body.insertAdjacentElement("beforeend", canvas)
+
+
+// TO DO LIST:
+// ball
+// paddle1 & paddle2
+// score
+// rounds?
+// update hit register function
+// change to opposite direction if hit function
+// winner function
+// key onclick direction function
