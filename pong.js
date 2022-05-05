d72f6c0db33c59b0e0b8522ef22480d721a15c70
GUSSCH <steamjg04@gmail.com>
2022-05-05 12:53:11 +0200

    .

--- pong.js
+++ pong.js
@@ -1,110 +1,175 @@
-const canvas = document.getElementById("pongGame")
-const context = canvas.getContext("2d")
-canvas.width = 650;
-canvas.height = 400;
+const canvas = document.getElementById("pongGame");
+const context = canvas.getContext("2d");
+canvas.width = 1250;
+canvas.height = 540;
 
-
-
-//var add = canvas.getContext("2d")
-//var canvas = document.createElement("canvas");
-//canvas.style = "position:absolute; left: 50%; width: 650px; height 400px; margin-left: -200px; border:1px solid #000000;";
-//document.body.insertAdjacentElement("beforeend", canvas)
-//console.log(canvas)
-
-
-class Element{
-    constructor(options){
-        this.x = options.x
-        this.y = options.y
-        this.width = options.width
-        this.height = options.height
-        this.color = options.color
-        this.speed = options.speed || 2
-        this.gravity = options.gravity
-        
-    }
+class Element {
+  constructor(options) {
+    this.x = options.x;
+    this.y = options.y;
+    this.width = options.width;
+    this.height = options.height;
+    this.color = options.color;
+    this.speed = options.speed || 2;
+    this.gravity = options.gravity;
+  }
 }
 
+//make ball bounce n shi
+function ballBounce() {
+  if (
+    ball.y + ball.gravity <= 0 ||
+    ball.y + ball.gravity >= canvas.height - 15
+  ) {
+    ball.gravity = ball.gravity * -1;
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  } else {
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  }
+  ballWallCollision();
+}
 
-function drawElements(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawElement(playerOne);
-    drawElement(playerTwo);
-    drawElement(ball);
+//detect collision
+function ballWallCollision() {
+  if (
+    ball.x + ball.speed <= 0 ||
+    ball.x + ball.speed + ball.width >= canvas.width
+  ) {
+    ball.y += ball.gravity;
+    ball.speed = ball.speed * -1;
+    ball.x += ball.speed;
+  } else {
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  }
+  drawElements();
+}
+
+function drawElements() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawElement(playerOne);
+  drawElement(playerTwo);
+  drawElement(ball);
 }
 
 //player 1
 const playerOne = new Element({
-    x: 10,
-    y: 200,
-    width: 15,
-    height: 80,
-    color: "#fff",
-    gravity: 2,
-})
+  x: 10,
+  y: canvas.height / 2 - 40,
+  width: 15,
+  height: 80,
+  color: "#fff",
+  gravity: 2,
+});
 
-//player 2 
+//player 2
 const playerTwo = new Element({
-    x: 625,
-    y: 200,
-    width: 15,
-    height: 80,
-    color: "#fff",
-    gravity: 2,
-})
+  x: canvas.width - 25,
+  y: canvas.height / 2 - 40,
+  width: 15,
+  height: 80,
+  color: "#fff",
+  gravity: 2,
+});
 
 //ball
 const ball = new Element({
-    x: 650 / 2,
-    y: 400 / 2,
-    width: 15,
-    height: 15,
-    color: "#20C20E",
-    speed: 1,
-    gravity: 1,
-})
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  width: 15,
+  height: 15,
+  color: "#20C20E",
+  speed: 8,
+  gravity: 4,
+});
 
-function bounce(){
-    if (ball.y + ball.gravity <= 0 || ball.y + ball.gravity >= canvas.height){
-        ball.gravity = ball.gravity * -1;
-        ball.y += ball.gravity;
-        ball.x += ball.speed;
-    }
-    else {
-        ball.y += ball.gravity;
-        ball.x += ball.speed;
-    }
-    collision();
+function bounce() {
+  if (ball.y + ball.gravity <= 0 || ball.y + ball.gravity >= canvas.height) {
+    ball.gravity = ball.gravity * -1;
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  } else {
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  }
+  collision();
 }
 // change to opposite direction if hit function
-function collision(){
-    if (ball.x + ball.speed <= 0 || ball.x + ball.speed + ball.width >= canvas.width){
-        ball.y += ball.gravity;
-        ball.speed = ball.speed -1;
-        ball.x += ball.speed;
-    }
-    else {
-        ball.y += ball.gravity;
-        ball.x += ball.speed; 
-    }
-    drawElements();
+function collision() {
+  if (
+    ball.x + ball.speed <= 0 ||
+    ball.x + ball.speed + ball.width >= canvas.width
+  ) {
+    ball.y += ball.gravity;
+    ball.speed = ball.speed - 1;
+    ball.x += ball.speed;
+  } else {
+    ball.y += ball.gravity;
+    ball.x += ball.speed;
+  }
+  drawElements();
 }
-
 
 function drawElement(element) {
-    context.fillStyle = element.color
-    context.fillRect(element.x, element.y, element.width, element.height)
+  context.fillStyle = element.color;
+  context.fillRect(element.x, element.y, element.width, element.height);
 }
 
-drawElement(playerOne)
-drawElement(playerTwo)
-drawElement(ball)
+function loop() {
+  ballBounce();
+  window.requestAnimationFrame(loop);
+}
+loop();
 
-// score
+drawElement(playerOne);
+drawElement(playerTwo);
+drawElement(ball);
 
-// rounds?
-// update hit register function
-// winner function
-// key onclick direction function
+//key movement
+window.addEventListener("keydown", doKeyDown, false);
+
+function doKeyDown(e) {
+  const key = e.key;
+  if (key == "w" && playerOne.y - playerOne.gravity > 0)
+    playerOne.y -= playerOne.gravity * 4;
+  else if (
+    key == "s" &&
+    playerOne.y + playerOne.height + playerOne.gravity < canvas.height
+  )
+    playerOne.y += playerOne.gravity * 4;
+
+  if (key == "i" && playerTwo.y - playerTwo.gravity > 0)
+    playerTwo.y -= playerTwo.gravity * 4;
+  else if (
+    key == "k" &&
+    playerTwo.y + playerTwo.height + playerTwo.gravity < canvas.height
+  )
+    playerTwo.y += playerTwo.gravity * 4;
+}
+
+function ballWallCollision() {
+  if (
+    (ball.y + ball.gravity <= playerTwo.y + playerTwo.height &&
+      ball.x + ball.width + ball.speed >= playerTwo.x &&
+      ball.y + ball.gravity > playerTwo.y) ||
+    (ball.y + ball.gravity > playerOne.y &&
+      ball.x + ball.speed <= playerOne.x + playerOne.width)
+  ) {
+    ball.speed = ball.speed * -1;
+  } else if (ball.x + ball.speed < playerOne.x) {
+    scoreTwo += 1;
+    ball.speed = ball.speed * -1;
+    ball.x = 100 + ball.speed;
+    ball.y += ball.gravity;
+  } else if (ball.x + ball.speed > playerTwo.x + playerTwo.width) {
+    scoreOne += 1;
+    ball.speed = ball.speed * -1;
+    ball.x = 100 + ball.speed;
+    ball.y += ball.gravity;
+  }
+  drawElements();
+}
 
 // Inspiration video: https://www.youtube.com/watch?v=IISA4XaE2gE
